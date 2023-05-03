91413b8d4bcf1713ab02d5bc22f61706fe1ba624
ehtisham <ehtishama876@gmail.com>
2023-05-03 19:29:17 +0500

    file comment

--- next.config.js
+++ next.config.js
@@ -4,20 +4,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-// const withTM = require('next-transpile-modules')(['three']);
-
-// module.exports = withTM({
-//   webpack: (config, options) => {
-//     config.module.rules.push({
-//       test: /\.(obj)$/i,
-//       loader: 'file-loader',
-//       options: {
-//         publicPath: '/_next/static/images/',
-//         outputPath: 'static/images/',
-//         name: '[name]-[hash].[ext]',
-//       },
-//     });
-
-//     return config;
-//   },
-// });
